154b4d48f26f632517a99ba5e8697797cb1b33cf
Iqbaalaji <ajieeevest@gmail.com>
2025-01-22 09:54:38 +0700

    update auth

--- AUTH/src/server.ts
+++ AUTH/src/server.ts
@@ -1,7 +1,15 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import authenticationRoutes from "./routes/authenticationRoutes";
 
 const server = Fastify();
+
+// Register CORS middleware
+server.register(cors, {
+  origin: "*", // Allow all origins (not recommended in production)
+  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+});
 
 server.register(authenticationRoutes, { prefix: "api/auth" });
 
