5dd19707590c682236334064204258f602159c80
Iqbaalaji <ajieeevest@gmail.com>
2025-01-22 11:26:00 +0700

    fix

--- AUTH/src/server.ts
+++ AUTH/src/server.ts
@@ -10,12 +10,18 @@ server.register(cors, {
   methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
 });
-
+server.get("/", function () {
+  return "Hello world!";
+});
 server.register(authenticationRoutes, { prefix: "api/auth" });
 
 const start = async () => {
   try {
-    await server.listen({ port: (process.env.PORT || 3000) as number });
+    // Set host to 0.0.0.0 and port to process.env.PORT or 3000
+    await server.listen({
+      host: "0.0.0.0",
+      port: Number(process.env.PORT) || 3000,
+    });
     console.log(`Server listening on port ${process.env.PORT || 3000}`);
   } catch (err) {
     server.log.error(err);
